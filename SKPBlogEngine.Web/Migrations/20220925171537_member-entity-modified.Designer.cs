514a1a7194a771a6dec64ec9e0fd182ad4b93353
Sachin Patel <sachin@skpatel.net>
2022-09-25 22:46:46 +0530

    member modification migration added

--- SKPBlogEngine.Web/Migrations/20220925171537_member-entity-modified.Designer.cs
+++ SKPBlogEngine.Web/Migrations/20220925171537_member-entity-modified.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SKPBlogEngine.Web.System;
 
@@ -10,9 +11,10 @@ using SKPBlogEngine.Web.System;
 namespace SKPBlogEngine.Web.Migrations
 {
     [DbContext(typeof(SKPDbContext))]
-    partial class SKPDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220925171537_member-entity-modified")]
+    partial class memberentitymodified
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
---
+++ SKPBlogEngine.Web/Migrations/20220925171537_member-entity-modified.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SKPBlogEngine.Web.Migrations
+{
+    public partial class memberentitymodified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Member",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Member",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DistplayName",
+                table: "Member",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Member",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Member");
+
+            migrationBuilder.DropColumn(
+                name: "DistplayName",
+                table: "Member");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Member");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Member",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
