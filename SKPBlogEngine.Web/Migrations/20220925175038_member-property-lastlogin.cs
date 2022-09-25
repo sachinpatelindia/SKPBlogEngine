ab06b61838d5f13ec6195c7d8685950f342eb771
Sachin Patel <sachin@skpatel.net>
2022-09-25 23:22:25 +0530

    member-lastlogin property migration added

---
+++ SKPBlogEngine.Web/Migrations/20220925175038_member-property-lastlogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SKPBlogEngine.Web.Migrations
+{
+    public partial class memberpropertylastlogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "Member",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLoginDate",
+                table: "Member");
+        }
+    }
+}
